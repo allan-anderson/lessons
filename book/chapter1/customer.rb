fabf3221eaebd39f74aec766c237449899a2345c
allansideas <allansideas@gmail.com>
2012-12-10 15:58:54 +0100

    add html statement

--- book/chapter1/customer.rb
+++ book/chapter1/customer.rb
@@ -11,19 +11,30 @@ class Customer
   end
 
   def statement
-    frequent_renter_points = 0
     result = "Rental Record for #{@name}\n"
 
     @rentals.each do |element|
-      frequent_renter_points += element.frequent_renter_points
-
       # show figures for this rental
       result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
     end
 
     # add footer lines
     result += "Amount owed is #{total_charge}\n"
-    result += "You earned #{frequent_renter_points} frequent renter points"
+    result += "You earned #{total_frequent_renter_points} frequent renter points"
+    result
+  end
+
+  def html_statement
+    result = "<h1>Rentals for <em>#{@name}</em></h1><p>\n"
+    @rentals.each do |element|
+      # show figures for this rental
+      result += "\t" + each.movie.title + ": " + element.charge.to_s + "<br>\n"
+    end
+    # add footer lines
+    result += "<p>You owe <em>#{total_charge}</em><p>\n"
+    result += "On this rental you earned " +
+      "<em>#{total_frequent_renter_points}</em> " +
+      "frequent renter points<p>"
     result
   end
 
@@ -31,6 +42,10 @@ class Customer
 
   def total_charge
     @rentals.inject(0) { |sum, rental| sum + rental.charge }
+  end
+
+  def total_frequent_renter_points
+    @rentals.inject(0) { |sum, rental| sum + rental.frequent_renter_points }
   end
 
 end
