ffe9eb4181ab1f0d60094d93064d16845a1cff42
allansideas <allansideas@gmail.com>
2012-12-10 16:31:51 +0100

    move frequent_rental_points also

--- book/chapter1/rental.rb
+++ book/chapter1/rental.rb
@@ -5,10 +5,6 @@ class Rental
     @movie, @days_rented = movie, days_rented
   end
 
-  def frequent_renter_points
-    (movie.price_code == Movie.NEW_RELEASE && days_rented > 1) ? 2 : 1
-  end
-
   def charge
     movie.charge(days_rented)
   end
