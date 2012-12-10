112f7d974383e35f0e001de8c97ef16aeefa7ef5
allansideas <allansideas@gmail.com>
2012-12-10 14:53:57 +0100

    renaming inside method for clarity

--- book/chapter1/customer.rb
+++ book/chapter1/customer.rb
@@ -34,18 +34,20 @@ class Customer
     result += "You earned #{frequent_renter_points} frequent renter points"
     result
   end
-  
-  def amount_for(element)
-    this_amount = 0
-    case element.movie.price_code
-      when Movie::REGULAR
-        this_amount += 2
-        this_amount += (element.days_rented - 2) * 1.5 if element.days_rented > 2
-      when Movie::NEW_RELEASE
-        this_amount += element.days_rented * 3
-      when Movie::CHILDRENS
-        this_amount += 1.5
-        this_amount += (element.days_rented - 3) * 1.5 if element.days_rented > 3
+
+  def amount_for(rental)
+    result = 0
+    case rental.movie.price_code
+    when Movie::REGULAR
+      result += 2
+      result += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
+    when Movie::NEW_RELEASE
+      result += rental.days_rented * 3
+    when Movie::CHILDRENS
+      result += 1.5
+      result += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
     end
+    result
   end
+
 end
